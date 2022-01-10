a8aaaf9f941c95653e1fe60e1183e05265a5c268
Lee Dong-geon <peter8739@gmail.com>
2022-01-11 01:35:16 +0900

    Create xlsx_to_txt.py

---
+++ xlsx_to_txt.py
@@ -0,0 +1,29 @@
+# !pip install openpyxl
+
+import pandas as pd
+import os
+
+INPUT_FILE_NAME = "Train_원본엑셀.xlsx"
+TEMP_FILE_NAME = "temp.txt"
+OUPUT_FILE_NAME = "input.txt"
+
+df = pd.read_excel(INPUT_FILE_NAME,
+                   sheet_name=0)
+
+with open(TEMP_FILE_NAME, 'w') as outfile:
+    df.to_string(outfile, index=False)
+
+# 임시로 TEMP_FILE을 만든 후, 삭제
+f_temp = open(TEMP_FILE_NAME, 'r')
+lines = f_temp.readlines()
+f_temp.close()
+os.remove(TEMP_FILE_NAME)
+
+f = open(OUPUT_FILE_NAME, 'w')
+for line in lines:
+    line = line.strip(" ")
+    line = line.replace("_x000D_\\n", " ")
+    line = line.replace("_x000D_", " ")
+    line = line + "\n\n"
+    f.write(line)
+f.close()
